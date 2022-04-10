4445748d2eab3c5d1891e94ba7a3104b004c14b6
DevPhantom <53095546+DevPhantom-Dev@users.noreply.github.com>
2022-04-10 17:29:44 +0800

    Update cateringdatabase.sql

--- databases/cateringdatabase.sql
+++ databases/cateringdatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2022 at 09:50 AM
+-- Generation Time: Apr 10, 2022 at 11:29 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -29,6 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `userdatabase` (
   `id` int(200) NOT NULL,
+  `name` text NOT NULL,
+  `email` text NOT NULL,
+  `phone` int(10) NOT NULL,
   `username` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -37,11 +40,13 @@ CREATE TABLE `userdatabase` (
 -- Dumping data for table `userdatabase`
 --
 
-INSERT INTO `userdatabase` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin'),
-(2, 'weqqewqw', 'qwewqewq'),
-(3, 'wqewqewqe', 'wqewqe'),
-(4, 'wewew', 'wewe');
+INSERT INTO `userdatabase` (`id`, `name`, `email`, `phone`, `username`, `password`) VALUES
+(1, '', '', 0, 'admin', 'admin'),
+(2, '', '', 0, 'weqqewqw', 'qwewqewq'),
+(3, '', '', 0, 'wqewqewqe', 'wqewqe'),
+(4, '', '', 0, 'wewew', 'wewe'),
+(5, '', '', 108569336, 'wqeqwe', 'qwewqe'),
+(6, '', 'wongyungen@gmail.com', 108569336, 'wqeq', 'wqeqe');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +66,7 @@ ALTER TABLE `userdatabase`
 -- AUTO_INCREMENT for table `userdatabase`
 --
 ALTER TABLE `userdatabase`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
