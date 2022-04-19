279b8f7e2664abd38a28280ac97d0e5f81d1e609
DevPhantom <53095546+DevPhantom-Dev@users.noreply.github.com>
2022-04-19 13:01:40 +0800

    Update cateringdatabase.sql

--- databases/cateringdatabase.sql
+++ databases/cateringdatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2022 at 11:29 AM
+-- Generation Time: Apr 19, 2022 at 07:00 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -24,14 +24,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `product_database`
+--
+
+CREATE TABLE `product_database` (
+  `foodID` int(11) NOT NULL,
+  `foodTitle` varchar(200) NOT NULL,
+  `foodDescription` varchar(500) NOT NULL,
+  `foodQuantity` int(200) NOT NULL,
+  `foodPrice` double NOT NULL,
+  `foodImageLoc` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product_database`
+--
+
+INSERT INTO `product_database` (`foodID`, `foodTitle`, `foodDescription`, `foodQuantity`, `foodPrice`, `foodImageLoc`) VALUES
+(1, 'Korean Food', 'Some quick example text to build on the card.', 10, 2.5, 'product1.jpg'),
+(2, 'American Burger', 'Dsdadadsadasdasdadwqewqeqwdsadas', 10, 5, 'product2.jpg'),
+(3, 'Kolo Mee', 'Nice food in Kuching to have', 10, 3, 'product3.jpg'),
+(4, 'Kolo Mee', 'Nice food in Kuching to have', 10, 3, 'product3.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `userdatabase`
 --
 
 CREATE TABLE `userdatabase` (
   `id` int(200) NOT NULL,
-  `name` text NOT NULL,
+  `name` varchar(200) NOT NULL,
   `email` text NOT NULL,
-  `phone` int(10) NOT NULL,
+  `phone` int(20) NOT NULL,
   `username` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -46,11 +71,22 @@ INSERT INTO `userdatabase` (`id`, `name`, `email`, `phone`, `username`, `passwor
 (3, '', '', 0, 'wqewqewqe', 'wqewqe'),
 (4, '', '', 0, 'wewew', 'wewe'),
 (5, '', '', 108569336, 'wqeqwe', 'qwewqe'),
-(6, '', 'wongyungen@gmail.com', 108569336, 'wqeq', 'wqeqe');
+(6, '', 'wongyungen@gmail.com', 108569336, 'wqeq', 'wqeqe'),
+(7, '', 'wongyungen@gmail.com', 108569336, 'qeqewqeqe', 'wqewqe'),
+(8, '', 'wongyungen@gmail.com', 108569336, 'qewqeqeqe', 'wqewqq'),
+(9, 'Yung En Wong', 'wongyungen@gmail.com', 108569336, 'wewqee', 'wqe'),
+(10, 'Yung En Wong', 'wongyungen@gmail.com', 108569336, 'wqwq', '123456'),
+(11, 'Yung En Wong', 'wongyungen@gmail.com', 108569336, 'qwqweqeq', '827ccb0eea8a706c4c34a16891f84e7b');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `product_database`
+--
+ALTER TABLE `product_database`
+  ADD PRIMARY KEY (`foodID`);
 
 --
 -- Indexes for table `userdatabase`
@@ -63,10 +99,16 @@ ALTER TABLE `userdatabase`
 --
 
 --
+-- AUTO_INCREMENT for table `product_database`
+--
+ALTER TABLE `product_database`
+  MODIFY `foodID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `userdatabase`
 --
 ALTER TABLE `userdatabase`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
