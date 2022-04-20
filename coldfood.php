35be62c7e8a7208ab0a8e4bd9f75c3e9a62523b5
JasonKho02 <101233218@students.swinburne.edu.my>
2022-04-20 21:37:04 +0800

    frozennFood

--- coldfood.php
+++ coldfood.php
@@ -16,8 +16,7 @@
     <meta name="author" content="Jason Kho" />
     <meta name="description" content="Cold Food Menu" />
     <link rel="stylesheet" type="text/css" href="styles/style.css" />
-    <script src="slide.js"></script> 
-    <script src="script.js"></script> 
+    <link rel="stylesheet" type="text/css" href="styles/foodstyle.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
@@ -47,35 +46,39 @@
     </div>
 </header>
 <article>
-    <div class="dropdown_article1">
-        <a class="dropbtn_good">&#8615; &#160; &#160; &#160;Types of Goods&#160; &#160; &#160; &#8615;</a>
-        <div class="dropdown-content_good">
-        <a href="coldfood.php">Frozen Goods</a>
-        <a href="warmfood.php">Warm Goods</a>
+        <div class="dropdown_article1">
+            <a class="dropbtn_good">&#8615; &#160; &#160; &#160;Types of Goods&#160; &#160; &#160; &#8615;</a>
+            <div class="dropdown-content_good">
+            <a href="coldfood.php">Frozen Goods</a>
+            <a href="warmfood.php">Warm Goods</a>
+            </div>
         </div>
-    </div>
 
-    <div class="dropdown_article2">
-        <a class="dropbtn_time">&#8615; &#160; &#160; &#160;Order Received Time&#160; &#160; &#160; &#8615;</a>
-        <div class="dropdown-content_time">
-            <a>11.00am</a>
-            <a>12.00pm</a>
-            <a>13.00pm</a>
-            <a>14.00pm</a>
-            <a>15.00pm</a>
-            <a>16.00pm</a>
-            <a>17.00pm</a>
-            <a>18.00pm</a>
-            <a>19.00am</a>
-            <a>20.00pm</a>
+        <div class="dropdown_article2">
+            <a class="dropbtn_time">&#8615; &#160; &#160; &#160;Order Received Time&#160; &#160; &#160; &#8615;</a>
+            <div class="dropdown-content_time">
+                <a>11.00am</a>
+                <a>12.00pm</a>
+                <a>13.00pm</a>
+                <a>14.00pm</a>
+                <a>15.00pm</a>
+                <a>16.00pm</a>
+                <a>17.00pm</a>
+                <a>18.00pm</a>
+                <a>19.00am</a>
+                <a>20.00pm</a>
+            </div>
         </div>
-    </div>
-    <form action="cart.php" class="input_address">
-        <label for="address" style="font-weight:bold">Deliver Address:</label>
-        <textarea id="address" name="address" rows="1" cols="50" placeholder="Provide Delivery Address"></textarea>
-    </form>
-
-    <p class="productNum"><?=$total_products?> Products Found</p>
+    
+        <form action="cart.php" class="input_address">
+            <label for="address" style="font-weight:bold">Deliver Address:</label>
+            <textarea id="address" name="address" rows="1" cols="50" placeholder="Provide Delivery Address"></textarea>
+        </form>
+    
+    <br />
+    <br />
+    <br />
+    <br />
     <div class="productContainer">
         <?php foreach ($products as $product): ?>
         <div class="card">
@@ -88,7 +91,7 @@
                 <h5>
                     <span class="price">RM<?=number_format($product['foodPrice'],2)?></span>
                 </h5>
-                <button type="submit" name="addCart">Add to Cart <i class="fas fa-shopping-cart"></i></button>
+                <button type="submit" name="addCart" class="addtoCart" style="font-size:24px">Add to Cart<i class="fa fa-shopping-cart"></i></button>
             </div>
         </div>
         <?php endforeach; ?>
