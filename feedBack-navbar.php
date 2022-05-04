a4468f7884e262f1ca298dee90308cb4e224a14e
JasonKho02 <101233218@students.swinburne.edu.my>
2022-05-04 22:28:40 +0800

    Feedback's Header

---
+++ feedBack-navbar.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/Feedback-Navbar.css">
+</head>
+<body>
+    <header>
+        <img href="#" class="logo" src="images/Logo.png" alt="logo"> 
+    </header>
+</body>
+</html>
