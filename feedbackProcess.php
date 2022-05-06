88e4d958291c0cf5c2b513945c14dccb5937b52d
JasonKho02 <101233218@students.swinburne.edu.my>
2022-05-06 09:45:24 +0800

    update feedbackProcess

---
+++ feedbackProcess.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+<head>
+	<title>Contact Form 07</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/style-FeedbackprocessPage.css">
+	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Source+Sans+Pro" rel="stylesheet">
+</head>
+
+<body>
+	<?php include_once 'feedBack-navbar.php'; ?>
+
+	<div class=content>
+		<div class="box">
+			<div class="box_wording">
+				<h1>Thank you !</h1>
+				<p>Thanks for submitting the Feedback Form. </p>
+				<a href="#" class="go-home">Home Page</a>
+			</div>
+		</div>
+	</div>
+<br>
+<br>
+<footer>
+	<?php include_once 'feedBack-footer.php'; ?>
+</footer>
+
+</body>
+</html>
