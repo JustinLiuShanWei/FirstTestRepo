f404c93003109c4ced8930b0a46189c39c3beeb2
DevPhantom <53095546+DevPhantom-Dev@users.noreply.github.com>
2022-05-19 00:42:03 +0800

    Update purchaseListingMainPage.php

--- purchaseListingMainPage.php
+++ purchaseListingMainPage.php
@@ -8,6 +8,7 @@
         while($orderArray=mysqli_fetch_array($result)){
             $datas[] = $orderArray;
         }
+        $test = sizeof($datas);
     }
 ?>
 
@@ -31,74 +32,75 @@
                 $priceArray = [];
                 $index = 0;
                 $previousID = -1;
-                if(!empty($dates)){
-                foreach($datas as $data){
-                    $orderId = $data["order_id"];
-                    $itemStatus = $data["order_status"];
-                    if ($index < sizeof($datas)-1){
-                        if ($datas[$index+1]["order_id"]==$orderId){
-                            $pause=true;
+                if(!empty($datas)){
+                    foreach($datas as $data){
+                        $orderId = $data["order_id"];
+                        $itemStatus = $data["order_status"];
+                        if ($index < sizeof($datas)-1){
+                            if ($datas[$index+1]["order_id"]==$orderId){
+                                $pause=true;
+                            }
+                            else $pause = false;
                         }
-                        else $pause = false;
-                    }
-                    if ($previousID!=$orderId){
-                    ?>
-                    <div class="context-section">
-                        <div class="orderContainer">
-                            <div class="individualOrderList">
-                                <div class="orderDetail-section">
-                                    <div class="orderDetail">
-                                        <a>Order ID: <span>#<?php echo $orderId?></span></a>
+                        if ($previousID!=$orderId){
+                        ?>
+                        <div class="context-section">
+                            <div class="orderContainer">
+                                <div class="individualOrderList">
+                                    <div class="orderDetail-section">
+                                        <div class="orderDetail">
+                                            <a>Order ID: <span>#<?php echo $orderId?></span></a>
+                                        </div>
+                                        <div class="orderStatus">
+                                            <a>Status: <span><?php echo $itemStatus?></span></a>
+                                        </div>
                                     </div>
-                                    <div class="orderStatus">
-                                        <a>Status: <span><?php echo $itemStatus?></span></a>
+                                </div>
+                        <?php 
+                        }   
+                            $itemID = $data["item_id"];
+                            $q = mysqli_query($conn,"SELECT ItemName FROM product_database WHERE ItemID='$itemID'");
+                            $item = mysqli_fetch_assoc($q);
+                            $itemQuantity = $data["item_quantity"];
+                            $itemPrice = $data["item_price"];
+                            array_push($priceArray,$itemPrice);
+                        ?>
+                            <div class="product-section">
+                                <div class="product-detail">
+                                    <div class="product-pic">
+                                        <img src="images/productImages/<?=$item["ItemImageLoc"]?>" alt="" class="card-image">
                                     </div>
+                                    <div class="product-desc" >
+                                        <span><?php echo $item["ItemName"]?></span><br>
+                                        <span>Quantity: <?php echo $itemQuantity?></span>
+                                    </div>
+                                    <div class="product-price">
+                                            <span><?php echo "RM ". number_format($itemPrice,2);?></span>
+                                    </div>
+                        <?php if ($pause!=true){ 
+                            foreach ($priceArray as $price) $total += $price;   
+                            $priceArray = []    
+                        ?>
+                            </div>
+                            </div>
+                            <div class="total-section">
+                                <div class="total-section-alignment">
+                                    <span>Order Total: </span>
+                                    <span class="total"> <?php echo "RM ". number_format($total,2);?></span>
                                 </div>
                             </div>
-                    <?php 
-                    }   
-                        $itemID = $data["item_id"];
-                        $q = mysqli_query($conn,"SELECT ItemName FROM product_database WHERE ItemID='$itemID'");
-                        $item = mysqli_fetch_assoc($q);
-                        $itemQuantity = $data["item_quantity"];
-                        $itemPrice = $data["item_price"];
-                        array_push($priceArray,$itemPrice);
-                    ?>
-                        <div class="product-section">
-                            <div class="product-detail">
-                                <div class="product-pic">
-                                    <img src="images/productImages/<?=$item["ItemImageLoc"]?>" alt="" class="card-image">
-                                </div>
-                                <div class="product-desc" >
-                                    <span><?php echo $item["ItemName"]?></span><br>
-                                    <span>Quantity: <?php echo $itemQuantity?></span>
-                                </div>
-                                <div class="product-price">
-                                        <span><?php echo "RM ". number_format($itemPrice,2);?></span>
-                                </div>
-                    <?php if ($pause!=true){ 
-                        foreach ($priceArray as $price) $total += $price;   
-                        $priceArray = []    
-                    ?>
-                        </div>
-                        </div>
-                        <div class="total-section">
-                            <div class="total-section-alignment">
-                                <span>Order Total: </span>
-                                <span class="total"> <?php echo "RM ". number_format($total,2);?></span>
-                            </div>
-                        </div>
-                    <?php 
-                        }
-                        $index += 1;
-                        $previousID=$orderId;
-                        
-                    ?>
-                    </div>
+                        <?php 
+                            }
+                            $index += 1;
+                            $previousID=$orderId;
                             
-                    </div>    
-                <?php 
-                }}
+                        ?>
+                        </div>
+                                
+                        </div>    
+                    <?php 
+                    }
+                }
                 else{
                     echo "<div class='no-records'>Your Cart is Empty</div><style>text-align=center;</style>";
                 }
